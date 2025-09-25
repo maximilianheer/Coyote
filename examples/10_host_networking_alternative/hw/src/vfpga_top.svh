3f04b3c358c7e4f32aaae9202428750f11048462
maximilianheer <mheer99@gmail.com>
2025-09-25 16:56:55 +0200

    Bugfix vFPGA, update program_hacc_local

--- examples/10_host_networking_alternative/hw/src/vfpga_top.svh
+++ examples/10_host_networking_alternative/hw/src/vfpga_top.svh
@@ -402,7 +402,11 @@ always_ff @(posedge aclk) begin
 
                         // Save the remainder for the next (and last) chunk
                         stream_remainder_data <= data_stream_data_out[511:480];
-                        stream_remainder_keep <= ((64'd1 << (64 - $coutones(data_stream_keep_out)))-1); // Shift the keep bits and add the 4 bits for the meta tag
+                        if($countones(data_stream_keep_out) < 64) begin 
+                            stream_remainder_keep <= ((64'd1 << (64 - $coutones(data_stream_keep_out)))-1); // Shift the keep bits and add the 4 bits for the meta tag
+                        end else begin 
+                            stream_remainder_keep <= 64'hf; 
+                        end  // Shift the keep bits and add the 4 bits for the meta tag
 
                         // Release the data chunk and the meta tag from their respective FIFOs
                         data_stream_ready_in <= 1'b1;
@@ -461,7 +465,11 @@ always_ff @(posedge aclk) begin
 
                             // Save the remainder for the next (and last) chunk
                             stream_remainder_data <= data_stream_data_out[511:480];
-                            stream_remainder_keep <= ((64'd1 << (64 - $coutones(data_stream_keep_out)))-1); // Shift the keep bits and add the 4 bits for the meta tag
+                            if($countones(data_stream_keep_out) < 64) begin 
+                                stream_remainder_keep <= ((64'd1 << (64 - $coutones(data_stream_keep_out)))-1); // Shift the keep bits and add the 4 bits for the meta tag
+                            end else begin 
+                                stream_remainder_keep <= 64'hf; 
+                            end 
 
                             // Release the data chunk and the meta tag from their respective FIFOs
                             data_stream_ready_in <= 1'b1;
--- scripts/ip_inst/network_infrastructure.tcl
+++ scripts/ip_inst/network_infrastructure.tcl
@@ -127,6 +127,10 @@ set_property -dict [list CONFIG.TDATA_NUM_BYTES {64} CONFIG.HAS_TKEEP {1} CONFIG
 create_ip -name ila -vendor xilinx.com -library ip -version 6.2 -module_name ila_host_networking
 set_property -dict [list CONFIG.C_PROBE3_WIDTH {512} CONFIG.C_PROBE4_WIDTH {64} CONFIG.C_PROBE8_WIDTH {512} CONFIG.C_PROBE9_WIDTH {64} CONFIG.C_NUM_OF_PROBES {10} CONFIG.C_EN_STRG_QUAL {1} CONFIG.ALL_PROBE_SAME_MU_CNT {2} CONFIG.C_DATA_DEPTH {8192}] [get_ips ila_host_networking]
 
+create_ip -name ila -vendor xilinx.com -library ip -version 6.2 -module_name ila_mux_host_sink
+set_property -dict [list CONFIG.C_PROBE3_WIDTH {512} CONFIG.C_PROBE4_WIDTH {64} CONFIG.C_PROBE8_WIDTH {512} CONFIG.C_PROBE9_WIDTH {64} CONFIG.C_PROBE12_WIDTH {32} CONFIG.C_PROBE13_WIDTH {28} CONFIG.C_PROBE14_WIDTH {28} CONFIG.C_NUM_OF_PROBES {18} CONFIG.C_EN_STRG_QUAL {1} CONFIG.ALL_PROBE_SAME_MU_CNT {2} CONFIG.C_DATA_DEPTH {8192}] [get_ips ila_mux_host_sink]
+
+
 create_ip -name ila -vendor xilinx.com -library ip -version 6.2 -module_name ila_host_networking_3_streams
 set_property -dict [list CONFIG.C_PROBE3_WIDTH {512} CONFIG.C_PROBE4_WIDTH {64} CONFIG.C_PROBE8_WIDTH {512} CONFIG.C_PROBE9_WIDTH {64} CONFIG.C_PROBE13_WIDTH {512} CONFIG.C_PROBE14_WIDTH {64} CONFIG.C_NUM_OF_PROBES {15} CONFIG.C_EN_STRG_QUAL {1} CONFIG.ALL_PROBE_SAME_MU_CNT {2} CONFIG.C_DATA_DEPTH {8192}] [get_ips ila_host_networking_3_streams]
 
