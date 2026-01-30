a9b909def65df55f0a1cea1559e5690246bd1ad3
Maximilian <hmaximili@ethz.ch>
2026-01-30 18:34:49 +0100

    Added the debugging ILA to the correct cnfg_slave... (damned be AVX)

--- hw/hdl/shell/cnfg_slave_avx.sv
+++ hw/hdl/shell/cnfg_slave_avx.sv
@@ -1026,6 +1026,24 @@ assign invldt_wr_ctrl.data.vaddr = slv_reg[ISR_REG][ISR_VADDR_OFFS+:VADDR_BITS];
 assign invldt_wr_ctrl.data.len = slv_reg[ISR_REG][ISR_LEN_OFFS+:LEN_BITS];
 assign invldt_wr_ctrl.data.last = slv_reg[ISR_REG][ISR_INVLDT_LAST];
 
+// ILA for observing the invalidate control signals
+ila_invldt_ctrl ila_invldt_ctrl_inst (
+    .clk(aclk),
+    .probe0(invldt_rd_ctrl.valid),      // 1 
+    .probe1(invldt_rd_ctrl.data.lock),  // 1 
+    .probe2(invldt_rd_ctrl.data.hpid),  // 32
+    .probe3(invldt_rd_ctrl.data.vaddr), // 64
+    .probe4(invldt_rd_ctrl.data.len),   // 28
+    .probe5(invldt_rd_ctrl.data.last),  // 1 
+    .probe6(invldt_wr_ctrl.valid),      // 1
+    .probe7(invldt_wr_ctrl.data.lock),  // 1
+    .probe8(invldt_wr_ctrl.data.hpid),  // 32
+    .probe9(invldt_wr_ctrl.data.vaddr), // 64
+    .probe10(invldt_wr_ctrl.data.len),  // 28
+    .probe11(invldt_wr_ctrl.data.last)  // 1
+);  
+
+
 // Pfault ctrl
 assign pfault_rd_ctrl.valid = slv_reg[ISR_REG][ISR_RESTART_RD];
 assign pfault_rd_ctrl.data = slv_reg[ISR_REG][ISR_SUCCESS]; 
